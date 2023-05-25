903c4ab610e8ae624201b60a0fde52045e441107
Alejandro S <yonseca@yonseca@posteo.net>
2023-05-25 17:00:47 +0200

    Tercer commit. Siguen quedando cosas por hacer, pero al menos ya escribimos en un fichero los datitos

--- src/main/java/FetchNotas.java
+++ src/main/java/FetchNotas.java
@@ -1,82 +1,67 @@
+import com.opencsv.bean.StatefulBeanToCsv;
+import com.opencsv.bean.StatefulBeanToCsvBuilder;
+import com.opencsv.exceptions.CsvDataTypeMismatchException;
+import com.opencsv.exceptions.CsvRequiredFieldEmptyException;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
-import org.jsoup.select.Elements;
 
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.Writer;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
-import java.time.Instant;
 import java.util.HashSet;
-import java.util.LinkedHashMap;
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.stream.Stream;
 import java.util.zip.GZIPInputStream;
 
 public class FetchNotas {
 
-    private static HashSet<Nota> notas = new HashSet<>();
-
-    public FetchNotas() {
-
-
-    }
+    public static final String URL = "https://old.meneame.net//notame/?page=";
+    private static final HashSet<Nota> notas = new HashSet<>();
+    public static final String FILE_NAME = "patata.csv";
 
     public static void main(String[] args) {
-        String url = "https://old.meneame.net//notame/?page="; // añadir página
         String maxPages = "1";
 
-
         try {
-            URI uri = new URI(url + maxPages);
+            URI uri = new URI(URL + maxPages);
             HttpClient httpClient = HttpClient.newBuilder().followRedirects(HttpClient.Redirect.NORMAL).build();
 
             HttpRequest request = HttpRequest.newBuilder(uri)
-                    //.header("Accept-Language", "*")
-                    //.header("User-Agent", "Mozilla/5.0 (X11; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/113.0")
                     .header("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8")
                     .header("Accept-Encoding", "gzip,deflate,br")
                     .build();
 
             HttpResponse<InputStream> response = httpClient.send(request, HttpResponse.BodyHandlers.ofInputStream());
             getNotas(response);
+            Writer writer = new FileWriter(FILE_NAME);
+            StatefulBeanToCsv<Nota> notaToCsv = new StatefulBeanToCsvBuilder<Nota>(writer).build();
+            notaToCsv.write(notas.stream());
             System.out.println(response.body());
 
-        } catch (URISyntaxException | IOException | InterruptedException e) {
+        } catch (URISyntaxException | IOException | InterruptedException | CsvRequiredFieldEmptyException |
+                 CsvDataTypeMismatchException e) {
             System.out.println("Boom: " + e.getMessage());
         }
     }
 
-    private static void getNotas(HttpResponse<InputStream> response){
+    private static void getNotas(HttpResponse<InputStream> response) {
         try (GZIPInputStream g = new GZIPInputStream(response.body())) {
             String string = new String(g.readAllBytes());
             Document doc = Jsoup.parse(string);
             var elements = doc.getElementsByAttributeValueStarting("data-id", "post-");
-
-            elements.parallelStream().forEach(e -> {
-                String timestamp, postId, user, referencedUsers, text;
-                Nota nota = new Nota();
-                Elements header = e.getElementsByClass("comment-header");
-                header.forEach(element -> {
-                    if (element.hasClass("username")){
-                        nota.setText(element.text());
-                        nota.setTimestamp(Instant.parse(element.attr("data-ts")));
-                    }
-                });
-                notas.add(nota);
-//                Elements text = e.getElementsByClass("comment-text");
-//                Elements footer = e.getElementsByClass("comment-footer");
-//                nota.setText(text.first().text());
-
-            });
-
-
-            System.out.println(string);
-        } catch (IOException io){
+            elements.forEach(e -> notas.add(new Nota(e)));
+        } catch (IOException io) {
             System.out.println("Boom: " + io.getMessage());
         }
+
+
+        notas.forEach((v) -> System.out.println(v.getUser() + ":" + v.getTimestamp() +  ":" + v.getText() + "\n"));
+
     }
+
+
 }
--- src/main/java/Nota.java
+++ src/main/java/Nota.java
@@ -1,23 +1,43 @@
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
 import java.time.Instant;
 import java.util.ArrayList;
 
-public class Nota {
+public class Nota implements Comparable<Nota>{
 
     public Nota(){}
 
-    public Nota(Instant timestamp, long postId, String user, ArrayList<String> referencedUsers, String text) {
+    public Nota(Element e){
+        System.out.println("Nueva nota" + this.toString());
+        getNotaHeaderData(e);
+        getNotaTextData(e);
+
+    }
+    public Nota(Instant timestamp, int postId, String user, ArrayList<String> referencedUsers, String text) {
         this.timestamp = timestamp;
         this.postId = postId;
         this.user = user;
         this.referencedUsers = referencedUsers;
         this.text = text;
+        this.elementToString = elementToString;
     }
 
-    Instant timestamp;
-    long postId;
-    String user;
-    ArrayList<String> referencedUsers;
-    String text;
+    private Instant timestamp;
+    private int postId;
+    private String user;
+    private ArrayList<String> referencedUsers;
+    private String text;
+
+    public String getElementToString() {
+        return elementToString;
+    }
+
+    public void setElementToString(String elementToString) {
+        this.elementToString = elementToString;
+    }
+
+    private String elementToString;
 
     public Instant getTimestamp() {
         return timestamp;
@@ -31,7 +51,7 @@ public class Nota {
         return postId;
     }
 
-    public void setPostId(long postId) {
+    public void setPostId(int postId) {
         this.postId = postId;
     }
 
@@ -57,5 +77,36 @@ public class Nota {
 
     public void setText(String text) {
         this.text = text;
+    }
+
+    @Override
+    public int compareTo(Nota o) {
+        return o.postId - this.postId;
+    }
+
+    private void getNotaHeaderData(Element notaElement) {
+        Elements header = notaElement.getElementsByClass("comment-header");
+        if (header.size() > 0) {
+            Elements headerContent = header.get(0).getAllElements();
+            for (var t : headerContent) {
+                if (t.hasClass("username")) {
+                    this.user = t.text();
+                }
+                if (t.hasAttr("data-ts")) {
+                    long ts = Long.parseLong(t.attr("data-ts"));
+                    this.timestamp = Instant.ofEpochSecond(ts);
+                }
+            }
+        }
+    }
+
+    private void getNotaTextData(Element notaElement) {
+        Elements body = notaElement.getElementsByClass("comment-text");
+        if (body.size() > 0) {
+            Element text = body.get(0);
+            this.text = text.wholeText();
+            elementToString = text.toString();
+            postId = Integer.parseInt(text.id().substring(4));
+        }
     }
 }
